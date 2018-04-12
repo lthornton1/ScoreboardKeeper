475b229e354402e71e534d6b1d887272fd1ab0fb
lthornton1 <lthornton1@mail.csuchico.edu>
2018-04-12 12:10:30 -0700

    done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include<cstdlib>
+#include<ctime>
 
 using namespace std;
 
@@ -8,12 +10,15 @@ const int MAX_TEAMS = 4;
 const int MIN_PERIODS = 1;
 const int MIN_TEAMS = 1;
 
+void printScoreboard(vector < vector <int> >);
+int randomBetween(int, int);
+
 int main()
 {
   int periods;
   int teams;
   vector< vector<int> > board;
-  int player = 1;
+  srand((int) time(0));
 
   cout<<"How many competitors? ";
   cin>>teams;
@@ -37,26 +42,50 @@ int main()
       board[r].resize(periods);
     }
 
-    for (int r=0; r<teams; r++)
+    for (int r=0; r<board.size(); r++)
     {
-      for (int c=0; c<periods; c++)
+      for (int c=0; c<board[r].size(); c++)
       {
         board[r][c] = 0;
       }
     }
+    printScoreboard(board);
 
-    cout << "SCOREBOARD" << endl;
-    for (int r=0; r<teams; r++)
+    for (int r=0; r<board.size(); r++)
     {
-      cout << "Player " << player << ": ";
-      for (int c=0; c<periods; c++)
+      for (int c=0; c<board[r].size(); c++)
       {
-        cout << board[r][c] <<"|";
+        board[r][c] = randomBetween(5,5);
       }
-      cout << endl;
-      player++;
     }
+    printScoreboard(board);
     //once created, display the scoreboard
   }
   return 0;
+}
+
+void printScoreboard(vector < vector <int> > board)
+{
+  cout << "SCOREBOARD" << endl;
+  for (int r=0; r<board.size(); r++)
+  {
+    cout << "Player " << r+1 << ": ";
+    for (int c=0; c<board[r].size(); c++)
+    {
+      cout << board[r][c] <<"|";
+    }
+    cout << endl;
+  }
+}
+
+int randomBetween (int first, int second)
+{
+  if (first>second)
+  {
+    return second + rand()%(first-second+1);
+  }
+  else
+  {
+    return first + rand()%(first-second+1);
+  }
 }
